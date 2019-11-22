97aec8a03cd3dc67fc1dee1030c8edf5e43c8a34
IwonaPoleszak <ivonapoleszak@gmail.com>
2019-11-22 11:05:08 +0100

    usuniecie paska

--- app/src/main/java/com/example/osk/ui/login/GetLocation.java
+++ app/src/main/java/com/example/osk/ui/login/GetLocation.java
@@ -10,6 +10,8 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -31,6 +33,10 @@ public class GetLocation extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_start_stop);
