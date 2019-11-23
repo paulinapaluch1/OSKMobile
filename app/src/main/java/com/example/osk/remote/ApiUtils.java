35e7d20f9b5bf72deec91879aa70ff50dc708d31
Paulina <paulina.paluch1@pollub.edu.pl>
2019-11-23 17:37:49 +0100

    logowanie do aplikacji mobilnej

---
+++ app/src/main/java/com/example/osk/remote/ApiUtils.java
@@ -0,0 +1,8 @@
+package com.example.osk.remote;
+
+public class ApiUtils {
+    public static final String BASE_URL="http://192.168.1.11:7070/mobile/";
+    public static UserService getUserService(){
+        return RetrofitClient.getClient(BASE_URL).create(UserService.class);
+    }
+}
