c3a8f9f439868095c7f8965036bd3ab68820a02f
Paulina <paulina.paluch1@pollub.edu.pl>
2019-11-24 09:39:52 +0100

    logowanie cd i zmiany na drugim ekranie

---
+++ app/src/main/java/com/example/osk/model/Instructor.java
@@ -0,0 +1,31 @@
+package com.example.osk.model;
+
+public class Instructor {
+    private String message;
+    private String name;
+    private String surname;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
--- app/src/main/java/com/example/osk/ui/login/GetLocation.java
+++ app/src/main/java/com/example/osk/ui/login/GetLocation.java
@@ -13,10 +13,12 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -29,14 +31,11 @@ public class GetLocation extends AppCompatActivity {
     private TextView t;
     private LocationManager locationManager;
     private LocationListener listener;
-
+    private String instructorNameAndSurname;
+    private EditText instructor;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
-        getSupportActionBar().hide(); // hide the title bar
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
         getSupportActionBar().hide(); // hide the title bar
@@ -47,7 +46,7 @@ public class GetLocation extends AppCompatActivity {
         t = (TextView) findViewById(R.id.textView);
         buttonStart = (Button) findViewById(R.id.buttonStart);
         buttonStop = (Button) findViewById(R.id.buttonStop);
-
+        instructor = (EditText) findViewById(R.id.instructor);
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
 
@@ -66,21 +65,24 @@ public class GetLocation extends AppCompatActivity {
             public void onProviderEnabled(String s) {
 
             }
-
             @Override
             public void onProviderDisabled(String s) {
-
                 Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
                 startActivity(i);
             }
         };
 
         configure_button();
+
+        Bundle extras = getIntent().getExtras();
+        if(extras!=null){
+            instructor.setText(instructor.getText()+ " "+instructorNameAndSurname);
+        }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode){
+        switch (requestCode) {
             case 10:
                 configure_button();
                 break;
@@ -89,20 +91,22 @@ public class GetLocation extends AppCompatActivity {
         }
     }
 
-    void configure_button(){
+    void configure_button() {
         // first check for permissions
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
-                        ,10);
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}
+                        , 10);
             }
             return;
         }
-        // this code won't execute IF permissions are not allowed, because in the line above there is return statement.
         buttonStart.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View view) {
-                //noinspection MissingPermission
+                if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
                 locationManager.requestLocationUpdates("gps", 5000, 0, listener);
             }
         });
