6d3b32786f3f1ea9751f0261c5bc37bdfa4d9052
Paulina <paulina.paluch1@pollub.edu.pl>
2019-12-01 17:45:21 +0100

    przesylanie wspolrzednych na serwer

--- app/src/main/java/com/example/osk/remote/UserService.java
+++ app/src/main/java/com/example/osk/remote/UserService.java
@@ -1,22 +1,24 @@
 package com.example.osk.remote;
+
 import com.example.osk.model.Instructor;
-import com.example.osk.model.Location;
-import java.util.List;
+import com.example.osk.model.LocationToSend;
+import com.example.osk.model.Message;
+
+import java.util.ArrayList;
+
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
-import retrofit2.http.POST;
+import retrofit2.http.HTTP;
 import retrofit2.http.Path;
 
 public interface UserService {
 
     @GET("login/{username}/{password}")
     Call<Instructor> login(@Path("username") String username,
-                           @Path("password") String password);
+              @Path("password") String password);
 
-    @POST("sendCoordinates/{coordinates}/")
-    Call<Boolean> sendCoordinates(@Path("coordinates") List<Location> coordinates);
-
-
-
+   @HTTP(method = "POST", path="send", hasBody = true)
+   Call<Message> sendCoordinates(@Body ArrayList<LocationToSend> coordinates);
 
 }
